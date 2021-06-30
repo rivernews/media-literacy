eadffd45b4ff4c4f6f5f98e2bd44b9109e54b2a7
Shaung <shaungc@umich.edu>
2021-06-30 05:01:35 +0000

    add some doc

--- lambda/src/slack_command_controller.py
+++ lambda/src/slack_command_controller.py
@@ -10,6 +10,8 @@ def lambda_handler(event, context):
         print('Hello there!', event, context)
 
         if not SignatureVerifier(os.environ.get('SLACL_SIGNING_SECRET', '')).is_valid_request(
+            # Validating AWS Lambda's Event Slack Request
+            # https://gist.github.com/nitrocode/288bb104893698011720d108e9841b1f
             base64.b64decode(event.get('body', '')).decode("utf-8") if event.get('isBase64Encoded') else event.get('body', ''),
             event.get('headers')
         ):
