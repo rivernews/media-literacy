89c666cabc2ca0f64dc9d1d59126768118850b22
Shaung <shaungc@umich.edu>
2022-09-14 11:13:41 -0700

    upgrade so project runs on M1

--- cloud_module/version.tf
+++ cloud_module/version.tf
@@ -1,7 +1,9 @@
 terraform {
-  required_version = "= 1.0.0"
+  required_version = "= 1.0.2" # minimum version that supports M1
 
   required_providers {
-    aws = "= 3.46.0"
+    # please use env var to pass over credentials
+    # https://registry.terraform.io/providers/hashicorp/aws/latest/docs
+    aws = "= 4.9.0" # minimum version based on `terraform init` error message
   }
 }
