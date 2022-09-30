09173316072bb026a22ae3338fa754f4328ce128
Shaung Cheng <shaungc@umich.edu>
2022-09-30 01:33:49 -0700

    Fix s3 trigger not working - S3 notification can only have one resource

--- cloud_module/pipeline/landing_s3_trigger.tf
+++ cloud_module/pipeline/landing_s3_trigger.tf
@@ -1,23 +1,3 @@
-resource "aws_s3_bucket_notification" "bucket_notification_landing_s3_trigger" {
-  bucket = data.aws_s3_bucket.archive.id
-
-  lambda_function {
-    lambda_function_arn = module.landing_s3_trigger_lambda.lambda_function_arn
-    // https://docs.aws.amazon.com/AmazonS3/latest/userguide/notification-how-to-event-types-and-destinations.html
-    events              = [
-      # "s3:ObjectCreated:Put",
-      # "s3:ObjectCreated:CompleteMultipartUpload",
-      "s3:ObjectCreated:*"
-    ]
-    filter_prefix       = "${local.newssite_economy_alias}/"
-    filter_suffix       = "/landing.html"
-  }
-
-  depends_on = [
-    aws_lambda_permission.allow_bucket_trigger_by_landing
-  ]
-}
-
 resource "aws_lambda_permission" "allow_bucket_trigger_by_landing" {
   statement_id  = "AllowExecutionFromS3Bucket"
   action        = "lambda:InvokeFunction"
